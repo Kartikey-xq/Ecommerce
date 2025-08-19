c26cdea7d9b24133d3cbb6f34e2b2f3ebb59ab3c
Kartikey <124981783+Kartikey-xq@users.noreply.github.com>
2025-08-19 17:31:31 +0530

    Update auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,14 +1,14 @@
 import api from "./api";
 
 export const registerUser = async(userData)=>{
-    const response = await api.post(`${baseUrl}/register`, userData,{
+    const response = await api.post(`/auth/register`, userData,{
         withCredentials:true,
     })
     return response.data;
 }
 
 export const loginUser = async(userData)=>{
-    const response = await api.post(`${baseUrl}/login`, userData,{
+    const response = await api.post(`/auth/login`, userData,{
         withCredentials:true,
     })
     return response.data;
