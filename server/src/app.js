1f1c0803faf6da162c10f11fd2142d94786c376d
Kartikey <124981783+Kartikey-xq@users.noreply.github.com>
2025-08-20 15:53:40 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: https://ecommerce-git-main-kartikeys-projects-2fe7d9d3.vercel.app',
+    origin: [https://ecommerce-git-main-kartikeys-projects-2fe7d9d3.vercel.app',https://ecommerce-two-ebon-61.vercel.app],
     credentials: true
 }));
 app.use(cookieParser());
